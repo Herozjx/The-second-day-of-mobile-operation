60fd956d77d6bb6ac1fe43f0f4a19ebb77b3ec3e
zjx <1193028309@qq.com>
2022-11-08 20:29:45 +0800

    作业三jd首页完成提交

--- 作业三：jd移动首页/js/setrem.js
+++ 作业三：jd移动首页/js/setrem.js
@@ -2,9 +2,13 @@ function setRem() {
   var ui_w = 375;
   var client_w =
     document.documentElement.clientWidth || document.body.clientWidth;
-
+  client_w = client_w > 375 ? 375 : client_w;
   var html = document.querySelector("html");
   html.style.fontSize = (client_w / ui_w) * 10 + "px";
 }
-
-window.onresize = setRem;
+setRem();
+var timer = null;
+window.onresize = function () {
+  clearTimeout(timer);
+  timer = setTimeout(setRem, 50);
+};
