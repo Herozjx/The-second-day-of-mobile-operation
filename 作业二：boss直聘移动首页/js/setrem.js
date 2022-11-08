fa4eef6140a77b976ea6e89ed315517fad498973
zjx <1193028309@qq.com>
2022-11-08 18:54:36 +0800

    作业二Boss直聘完成提交

--- 作业二：boss直聘移动首页/js/setrem.js
+++ 作业二：boss直聘移动首页/js/setrem.js
@@ -2,9 +2,14 @@ function setRem() {
   var ui_w = 640;
   var client_w =
     document.documentElement.clientWidth || document.body.clientWidth;
-
+  client_w = client_w < 310 ? 310 : client_w;
   var html = document.querySelector("html");
   html.style.fontSize = (client_w / ui_w) * 10 + "px";
+  console.log(client_w);
 }
 setRem();
-window.onresize = setRem;
+var timer = null;
+window.onresize = function () {
+  clearTimeout(timer);
+  timer = setTimeout(setRem, 50);
+};
