b7f0f78916812086ae2f1042d47ded4008d77966
zjx <1193028309@qq.com>
2022-11-08 17:34:53 +0800

    作业二boss头部完成提交

--- 作业二：boss直聘移动首页/js/setrem.js
+++ 作业二：boss直聘移动首页/js/setrem.js
@@ -1,10 +1,10 @@
 function setRem() {
-  var ui_w = 375;
+  var ui_w = 640;
   var client_w =
     document.documentElement.clientWidth || document.body.clientWidth;
 
   var html = document.querySelector("html");
   html.style.fontSize = (client_w / ui_w) * 10 + "px";
 }
-
+setRem();
 window.onresize = setRem;
