51f22f1041f5338d971f154b7540d10b48de2f87
zjx <1193028309@qq.com>
2022-11-08 22:57:58 +0800

    作业五携程网完成提交结束

--- 作业五：携程网-自适应/js/setrem.js
+++ 作业五：携程网-自适应/js/setrem.js
@@ -1,10 +1,16 @@
 function setRem() {
   var ui_w = 375;
-  var client_w =
-    document.documentElement.clientWidth || document.body.clientWidth;
-
+  client_w = document.documentElement.clientWidth || document.body.clientWidth;
+  client_w = client_w > 500 ? 500 : client_w;
+  client_w = client_w < 250 ? 250 : client_w;
   var html = document.querySelector("html");
   html.style.fontSize = (client_w / ui_w) * 10 + "px";
+  // console.log(client_w);
 }
-
-window.onresize = setRem;
+setRem();
+// 防抖
+var timer = null;
+window.onresize = function () {
+  clearTimeout(timer);
+  timer = setTimeout(setRem, 50);
+};
